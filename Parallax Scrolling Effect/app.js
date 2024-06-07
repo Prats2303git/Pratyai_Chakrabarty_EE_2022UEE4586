32b7f881c22035e3db4d3a2e8753207c06791a85
Pratyai Chakrabarty <124715995+Prats2303git@users.noreply.github.com>
2024-06-07 13:58:08 +0530

    Add files via upload

---
+++ Parallax Scrolling Effect/app.js
@@ -0,0 +1,106 @@
+gsap.to(".bg",{
+    scrollTrigger:{
+        trigger : ".page1",
+        scrub : 1,
+        pin : true
+    },
+    scale : 1.5
+});
+gsap.to(".man",{
+    scrollTrigger:{
+        scrub : 1,
+    },
+    scale : 0.5
+});
+gsap.to(".mtn-left",{
+    scrollTrigger:{
+        scrub : 1
+    },
+    x : -500
+});
+gsap.to(".mtn-right",{
+    scrollTrigger:{
+        scrub : 1,
+    },
+    x : 500
+});
+gsap.to(".clouds1",{
+    scrollTrigger:{
+        scrub : 1,
+    },
+    x : -150
+});
+gsap.to(".clouds2",{
+    scrollTrigger:{
+        scrub : 1,
+    },
+    x : 150
+});
+gsap.to(".page1 h1",{
+    scrollTrigger:{
+        scrub : 1,
+    },
+    y : -400
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
